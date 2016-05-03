3f7da11ae49bd46491ec88818d8bcf12ebdeda45
Tanuj Mittal <tanuj.183@gmail.com>
2016-05-03 14:33:01 -0700

    Fix streams

--- src/main/java/dsblog/Database.java
+++ src/main/java/dsblog/Database.java
@@ -1,27 +1,28 @@
 package dsblog;
 
 import common.Constants;
+import utils.CommonUtils;
 import utils.LogUtils;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Database {
 
     private String LOG_TAG = "Database";
     private FileOutputStream dbWriter;
-    private FileInputStream dbReader;
     private int counter;
     private String DB_FILE;
 
-    Database(int serverID){
+    Database(int serverID) {
         LOG_TAG += "-" + serverID;
         DB_FILE = Constants.DB_FILE + "-" + serverID;
         counter = 0;
-        try{
+        try {
             dbWriter = new FileOutputStream(DB_FILE, true); // Create a new file whether it exists or not.
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             LogUtils.error(LOG_TAG, "Failed to initialize DB file.", e);
             System.exit(-1);
         }
@@ -30,14 +31,16 @@ public class Database {
     public void insert(String blogPost) throws IOException {
         //dbWriter.write(("Post number " + ++counter + ". " + blogPost + Constants.OBJECT_DELIMITER).getBytes());
         dbWriter.write((blogPost + Constants.OBJECT_DELIMITER).getBytes());
+        dbWriter.flush();
 
         LogUtils.debug(LOG_TAG, "Written new post to file. Counter = " + counter);
     }
 
     public String lookUp() throws IOException {
-        dbReader = new FileInputStream(DB_FILE);
-        byte[] file = new byte[(int)(new File(DB_FILE).length())];
+        FileInputStream dbReader = new FileInputStream(DB_FILE);
+        byte[] file = new byte[(int) (new File(DB_FILE).length())];
         dbReader.read(file);
+        CommonUtils.closeQuietly(dbReader);
         LogUtils.debug(LOG_TAG, "Look up successful.");
         return new String(file);
     }
--- src/main/java/dsblog/Log.java
+++ src/main/java/dsblog/Log.java
@@ -1,27 +1,28 @@
 package dsblog;
 
 import common.Constants;
+import utils.CommonUtils;
 import utils.LogUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Log {
 
     private String LOG_TAG = "Log";
     private FileOutputStream logWriter;
-    private FileInputStream logReader;
     private String LOG_FILE;
 
-    public Log(int serverID){
+    public Log(int serverID) {
         LOG_TAG += "-" + serverID;
         LOG_FILE = Constants.LOG_FILE + "-" + serverID;
-        try{
+        try {
             logWriter = new FileOutputStream(LOG_FILE); // Create a new file each time.
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             LogUtils.error(LOG_TAG, "Failed to initialize Log file.", e);
             System.exit(-1);
         }
@@ -29,17 +30,19 @@ public class Log {
 
     public void append(Event e) throws IOException {
         logWriter.write((e.toString() + Constants.OBJECT_DELIMITER).getBytes());
+        logWriter.flush();
         LogUtils.debug(LOG_TAG, "Appended new event to log.");
     }
 
-    public List<Event> readLog() throws IOException{
-        logReader = new FileInputStream(LOG_FILE);
+    public List<Event> readLog() throws IOException {
+        FileInputStream logReader = new FileInputStream(LOG_FILE);
         byte[] file = new byte[(int) new File(LOG_FILE).length()];
         List<Event> events = new ArrayList<>();
         logReader.read(file);
+        CommonUtils.closeQuietly(logReader);
 
         String[] file_str = (new String(file)).split(Constants.OBJECT_DELIMITER);
-        for(String s : file_str)
+        for (String s : file_str)
             events.add(Event.fromString(s));
 
         LogUtils.debug(LOG_TAG, "Read " + events.size() + " events from log.");
