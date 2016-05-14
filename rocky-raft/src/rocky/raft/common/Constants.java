b25b7175135ba4dc133978c8a5a2cdabf70a663b
Tanuj Mittal <tanuj.183@gmail.com>
2016-05-13 22:25:15 -0700

    Update Constants

--- rocky-raft/src/rocky/raft/common/Constants.java
+++ rocky-raft/src/rocky/raft/common/Constants.java
@@ -3,7 +3,9 @@ package rocky.raft.common;
 
 public class Constants {
 
-    public static final int TIMEOUT_MAX = 300; // In milliseconds.
+    public static final long TIMEOUT_MAX = 300; // In milliseconds.
 
-    public static final int TIMEOUT_MIN = 150; // In milliseconds.
+    public static final long TIMEOUT_MIN = 150; // In milliseconds.
+
+    public static final long HEARTBEAT_DELAY = 100;
 }
